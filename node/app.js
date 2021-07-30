1a2f99a064c7640c1bce231ebb433e5e074179a8
sautmanurung1 <smanurung360@gmail.com>
2021-07-30 22:41:22 +0700

    perubahan baru dan penambahan

---
+++ node/app.js
@@ -0,0 +1,13 @@
+const http = require('http');
+const server = http.createServer((req,res)=>{
+    if(req.url === '/'){
+        res.write('Hello world from nodejs');
+        res.end();
+    }
+    else{
+        res.write('Using some other domain');
+        res.end();
+    }
+});
+
+server.listen('3000');
---
+++ node/static file/app.js
@@ -0,0 +1,8 @@
+const http = require('http');
+const fs = require('fs');
+
+http.createServer((req,res)=>{
+    const readStream = fs.createReadStream('./static/index.html');
+    res.writeHead(200,{'content-type' : 'text/html'});
+    readStream.pipe(res);
+}).listen(3000);
